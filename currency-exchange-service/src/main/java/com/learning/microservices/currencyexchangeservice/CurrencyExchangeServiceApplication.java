42634f66f04178baaaddaa46924da5dd6ba4b238
bgarapati-git <58849852+bgarapati-git@users.noreply.github.com>
2020-08-20 13:29:28 +0530

    Update CurrencyExchangeServiceApplication.java

--- currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,9 +3,9 @@ package com.learning.microservices.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 
-import brave.sampler.Sampler;
+//import brave.sampler.Sampler;
 //import io.micrometer.core.instrument.LongTaskTimer.Sample;
 
 @SpringBootApplication
@@ -16,9 +16,9 @@ public class CurrencyExchangeServiceApplication {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
 	
-	@Bean
-	public Sampler defaultSampler() {
-		return Sampler.ALWAYS_SAMPLE;
-	}
+//	@Bean
+//	public Sampler defaultSampler() {
+//		return Sampler.ALWAYS_SAMPLE;
+//	}
 
 }
